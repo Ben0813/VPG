84ccc28903ccf55e0f1ed2fec04f3385744a054b
Benoit Dewigne <benoitdewigne@gmail.com>
2023-07-12 16:55:14 +0200

    tailwind init

---
+++ front/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        raisin: "#2C2B3C",
+        onyx: "#403F4C",
+        metal: "#1B2432",
+        rich: "#121420",
+        khaki: "#A99985",
+      },
+      fontFamily: {
+        rajdhani: ["Rajdhani", "sans-serif"],
+        barlow: ["Barlow", "sans-serif"],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
