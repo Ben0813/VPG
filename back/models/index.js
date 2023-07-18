32f946ffdb7bed96f796fc7cb06d3e9a61133ae5
Benoit Dewigne <benoitdewigne@gmail.com>
2023-07-18 15:58:48 +0200

    fix vulnerability

--- back/models/index.js
+++ back/models/index.js
@@ -1,10 +1,12 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const sequelize = new Sequelize({
-  database: "database_development",
-  username: "root",
-  password: null,
-  host: "127.0.0.1",
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
   dialect: "mysql",
 });
 
