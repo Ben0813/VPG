56e6f7cea709f1601a1a845fb2165ee1647ea0e3
Benoit Dewigne <benoitdewigne@gmail.com>
2023-07-20 01:30:26 +0200

    fix index.js for development

--- back/models/index.js
+++ back/models/index.js
@@ -1,7 +1,23 @@
-import { Sequelize } from "sequelize";
+/*import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
 export const sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL);
+
+export default sequelize;
+
+*/
+
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const sequelize = new Sequelize({
+  database: process.env.DB_NAME,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  dialect: "mysql",
+});
 
 export default sequelize;
